22d27c16397cfb94125a49ee564060de8bfc0c24
Tami <kazehare.t@gmail.com>
2024-02-21 12:33:10 +0000

    comment_count included in article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const data = require("../db/data/test-data/index");
 const db = require("../db/connection");
 const request = require("supertest");
 const app = require("../app");
+const { convertTimestampToDate } = require("../db/seeds/utils");
 
 beforeEach(() => seed(data));
 afterAll(() => db.end());
@@ -173,63 +174,94 @@ describe("GET /api/articles/:article_id", () => {
       });
   });
 
-  describe("GET /api/articles/:article_id/comments", () => {
-    test("GET 200 - responds with an array of comments for the given article_id of which each comment should have the relevant properties", () => {
+  describe("GET /api/articles/:article_id (comment_count)", () => {
+    test("GET 200 /api/articles/:article_id - responds an article object, which should have the relevant properties including a comment_count", () => {
+      const articleId = 9;
       return request(app)
-        .get(`/api/articles/1/comments`)
+        .get(`/api/articles/${articleId}`)
         .expect(200)
-        .then(({ body: { comments } }) => {
-          expect(Array.isArray(comments)).toBe(true);
-          expect(comments.length).toBe(11);
-          comments.forEach((comment) => {
-            expect(comment).toMatchObject({
-              comment_id: expect.any(Number),
-              votes: expect.any(Number),
-              created_at: expect.any(String),
-              author: expect.any(String),
-              body: expect.any(String),
-              article_id: 1,
-            });
+        .then(({ body: { article } }) => {
+          expect(article).toMatchObject({
+            article_id: articleId,
+            created_at: convertTimestampToDate(1591438200000),
+            comment_count: expect.any(Number),
+            comment_count: 2,
           });
         });
     });
-    test("GET 200 - responds with the most recent comments first", () => {
+    test("GET 200 /api/articles/:article_id - if comment is not yet made for referenced article", () => {
+      const articleId = 2;
       return request(app)
-        .get(`/api/articles/1/comments`)
+        .get(`/api/articles/${articleId}`)
         .expect(200)
-        .then(({ body: { comments } }) => {
-          expect(comments).toBeSortedBy("created_at", {
-            descending: true,
+        .then(({ body: { article } }) => {
+          expect(article).toMatchObject({
+            article_id: articleId,
+            created_at: convertTimestampToDate(1602828180000),
+            comment_count: expect.any(Number),
+            comment_count: 0,
           });
         });
     });
-    test("GET 404 -- responds with approriate status and message, when the request has invalid article id number", () => {
-      const articleId = 999;
-      return request(app)
-        .get(`/api/articles/${articleId}/comments`)
-        .expect(404)
-        .then(({ body: { msg } }) => {
-          expect(msg).toBe("Not Found");
+  });
+});
+
+describe("GET /api/articles/:article_id/comments", () => {
+  test("GET 200 - responds with an array of comments for the given article_id of which each comment should have the relevant properties", () => {
+    return request(app)
+      .get(`/api/articles/1/comments`)
+      .expect(200)
+      .then(({ body: { comments } }) => {
+        expect(Array.isArray(comments)).toBe(true);
+        expect(comments.length).toBe(11);
+        comments.forEach((comment) => {
+          expect(comment).toMatchObject({
+            comment_id: expect.any(Number),
+            votes: expect.any(Number),
+            created_at: expect.any(String),
+            author: expect.any(String),
+            body: expect.any(String),
+            article_id: 1,
+          });
         });
-    });
-    test("GET 400 -- responds with approriate status and message, when the request has invalid article id type", () => {
-      const articleId = "papers";
-      return request(app)
-        .get(`/api/articles/${articleId}/comments`)
-        .expect(400)
-        .then(({ body: { msg } }) => {
-          expect(msg).toBe("Bad Request");
+      });
+  });
+  test("GET 200 - responds with the most recent comments first", () => {
+    return request(app)
+      .get(`/api/articles/1/comments`)
+      .expect(200)
+      .then(({ body: { comments } }) => {
+        expect(comments).toBeSortedBy("created_at", {
+          descending: true,
         });
-    });
-    test("GET 200 - responds with an empty array when a valid article id is passed but with no associated comments", () => {
-      return request(app)
-        .get(`/api/articles/2/comments`)
-        .expect(200)
-        .then(({ body: { comments } }) => {
-          expect(Array.isArray(comments)).toBe(true);
-          expect(comments.length).toBe(0);
-        });
-    });
+      });
+  });
+  test("GET 404 -- responds with approriate status and message, when the request has invalid article id number", () => {
+    const articleId = 999;
+    return request(app)
+      .get(`/api/articles/${articleId}/comments`)
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Not Found");
+      });
+  });
+  test("GET 400 -- responds with approriate status and message, when the request has invalid article id type", () => {
+    const articleId = "papers";
+    return request(app)
+      .get(`/api/articles/${articleId}/comments`)
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request");
+      });
+  });
+  test("GET 200 - responds with an empty array when a valid article id is passed but with no associated comments", () => {
+    return request(app)
+      .get(`/api/articles/2/comments`)
+      .expect(200)
+      .then(({ body: { comments } }) => {
+        expect(Array.isArray(comments)).toBe(true);
+        expect(comments.length).toBe(0);
+      });
   });
 });
 
