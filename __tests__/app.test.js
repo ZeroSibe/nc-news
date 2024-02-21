18bbce62d0027012a62752499e24fc401b334a4f
Tami <kazehare.t@gmail.com>
2024-02-21 09:16:13 +0000

    prepped app.test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -57,148 +57,148 @@ describe("GET /api", () => {
         });
     });
   });
+});
 
-  describe("GET /api/articles", () => {
-    test("GET 200 - responds with an articles array of article objects with the relevant properties", () => {
+describe("GET /api/articles", () => {
+  test("GET 200 - responds with an articles array of article objects with the relevant properties", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body: { articles } }) => {
+        expect(articles.length).toBe(13);
+        articles.forEach((article) => {
+          expect(article).toMatchObject({
+            author: expect.any(String),
+            title: expect.any(String),
+            article_id: expect.any(Number),
+            topic: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+            comment_count: expect.any(Number),
+          });
+        });
+      });
+  });
+
+  test("GET 200 sort articles by date if no query is provided", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body: { articles } }) => {
+        expect(articles).toBeSortedBy("created_at", {
+          descending: true,
+          coerce: true,
+        });
+      });
+  });
+
+  test("GET 404 - should return appropriate status and message for bad path", () => {
+    return request(app)
+      .get("/api/articless")
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Path Not Found");
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("GET 200 /api/articles/:article_id - responds an article object, which should have the relevant properties", () => {
+    const articleId = 2;
+    return request(app)
+      .get(`/api/articles/${articleId}`)
+      .expect(200)
+      .then(({ body: { article } }) => {
+        expect(article).toMatchObject({
+          article_id: articleId,
+          article_id: expect.any(Number),
+          title: "Sony Vaio; or, The Laptop",
+          topic: "mitch",
+          author: "icellusedkars",
+          body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+          created_at: "2020-10-16T05:03:00.000Z",
+          votes: 0,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+  test("GET 404 - responds with approriate status and message, when the request has invalid article id number", () => {
+    const articleId = 999;
+    return request(app)
+      .get(`/api/articles/${articleId}`)
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Not Found");
+      });
+  });
+  test("GET 400 - responds with approriate status and message, when the request has invalid article id type", () => {
+    const articleId = "forkLift";
+    return request(app)
+      .get(`/api/articles/${articleId}`)
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request");
+      });
+  });
+
+  describe("GET /api/articles/:article_id/comments", () => {
+    test("GET 200 - responds with an array of comments for the given article_id of which each comment should have the relevant properties", () => {
       return request(app)
-        .get("/api/articles")
+        .get(`/api/articles/1/comments`)
         .expect(200)
-        .then(({ body: { articles } }) => {
-          expect(articles.length).toBe(13);
-          articles.forEach((article) => {
-            expect(article).toMatchObject({
-              author: expect.any(String),
-              title: expect.any(String),
-              article_id: expect.any(Number),
-              topic: expect.any(String),
-              created_at: expect.any(String),
+        .then(({ body: { comments } }) => {
+          expect(Array.isArray(comments)).toBe(true);
+          expect(comments.length).toBe(11);
+          comments.forEach((comment) => {
+            expect(comment).toMatchObject({
+              comment_id: expect.any(Number),
               votes: expect.any(Number),
-              article_img_url: expect.any(String),
-              comment_count: expect.any(Number),
+              created_at: expect.any(String),
+              author: expect.any(String),
+              body: expect.any(String),
+              article_id: 1,
             });
           });
         });
     });
-
-    test("GET 200 sort articles by date if no query is provided", () => {
+    test("GET 200 - responds with the most recent comments first", () => {
       return request(app)
-        .get("/api/articles")
+        .get(`/api/articles/1/comments`)
         .expect(200)
-        .then(({ body: { articles } }) => {
-          expect(articles).toBeSortedBy("created_at", {
+        .then(({ body: { comments } }) => {
+          expect(comments).toBeSortedBy("created_at", {
             descending: true,
-            coerce: true,
           });
         });
     });
-
-    test("GET 404 - should return appropriate status and message for bad path", () => {
+    test("GET 404 -- responds with approriate status and message, when the request has invalid article id number", () => {
+      const articleId = 999;
       return request(app)
-        .get("/api/articless")
+        .get(`/api/articles/${articleId}/comments`)
         .expect(404)
         .then(({ body: { msg } }) => {
-          expect(msg).toBe("Path Not Found");
+          expect(msg).toBe("Not Found");
         });
     });
-
-    describe("GET /api/articles/:article_id", () => {
-      test("GET 200 /api/articles/:article_id - responds an article object, which should have the relevant properties", () => {
-        const articleId = 2;
-        return request(app)
-          .get(`/api/articles/${articleId}`)
-          .expect(200)
-          .then(({ body: { article } }) => {
-            expect(article).toMatchObject({
-              article_id: articleId,
-              article_id: expect.any(Number),
-              title: "Sony Vaio; or, The Laptop",
-              topic: "mitch",
-              author: "icellusedkars",
-              body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
-              created_at: "2020-10-16T05:03:00.000Z",
-              votes: 0,
-              article_img_url:
-                "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
-            });
-          });
-      });
-      test("GET 404 - responds with approriate status and message, when the request has invalid article id number", () => {
-        const articleId = 999;
-        return request(app)
-          .get(`/api/articles/${articleId}`)
-          .expect(404)
-          .then(({ body: { msg } }) => {
-            expect(msg).toBe("Not Found");
-          });
-      });
-      test("GET 400 - responds with approriate status and message, when the request has invalid article id type", () => {
-        const articleId = "forkLift";
-        return request(app)
-          .get(`/api/articles/${articleId}`)
-          .expect(400)
-          .then(({ body: { msg } }) => {
-            expect(msg).toBe("Bad Request");
-          });
-      });
-
-      describe("GET /api/articles/:article_id/comments", () => {
-        test("GET 200 - responds with an array of comments for the given article_id of which each comment should have the relevant properties", () => {
-          return request(app)
-            .get(`/api/articles/1/comments`)
-            .expect(200)
-            .then(({ body: { comments } }) => {
-              expect(Array.isArray(comments)).toBe(true);
-              expect(comments.length).toBe(11);
-              comments.forEach((comment) => {
-                expect(comment).toMatchObject({
-                  comment_id: expect.any(Number),
-                  votes: expect.any(Number),
-                  created_at: expect.any(String),
-                  author: expect.any(String),
-                  body: expect.any(String),
-                  article_id: 1,
-                });
-              });
-            });
+    test("GET 400 -- responds with approriate status and message, when the request has invalid article id type", () => {
+      const articleId = "papers";
+      return request(app)
+        .get(`/api/articles/${articleId}/comments`)
+        .expect(400)
+        .then(({ body: { msg } }) => {
+          expect(msg).toBe("Bad Request");
         });
-        test("GET 200 - responds with the most recent comments first", () => {
-          return request(app)
-            .get(`/api/articles/1/comments`)
-            .expect(200)
-            .then(({ body: { comments } }) => {
-              expect(comments).toBeSortedBy("created_at", {
-                descending: true,
-              });
-            });
+    });
+    test("GET 200 - responds with an empty array when a valid article id is passed but with no associated comments", () => {
+      return request(app)
+        .get(`/api/articles/2/comments`)
+        .expect(200)
+        .then(({ body: { comments } }) => {
+          expect(Array.isArray(comments)).toBe(true);
+          expect(comments.length).toBe(0);
         });
-        test("GET 404 -- responds with approriate status and message, when the request has invalid article id number", () => {
-          const articleId = 999;
-          return request(app)
-            .get(`/api/articles/${articleId}/comments`)
-            .expect(404)
-            .then(({ body: { msg } }) => {
-              expect(msg).toBe("Not Found");
-            });
-        });
-        test("GET 400 -- responds with approriate status and message, when the request has invalid article id type", () => {
-          const articleId = "papers";
-          return request(app)
-            .get(`/api/articles/${articleId}/comments`)
-            .expect(400)
-            .then(({ body: { msg } }) => {
-              expect(msg).toBe("Bad Request");
-            });
-        });
-        test("GET 200 - responds with an empty array when a valid article id is passed but with no associated comments", () => {
-          return request(app)
-            .get(`/api/articles/2/comments`)
-            .expect(200)
-            .then(({ body: { comments } }) => {
-              expect(Array.isArray(comments)).toBe(true);
-              expect(comments.length).toBe(0);
-            });
-        });
-      });
     });
   });
 });
